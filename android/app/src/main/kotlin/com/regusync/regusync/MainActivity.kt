47a7595db8df68846558abdc07f2f9e5e70ca32f
Onic Agustino <L200234275@student.ums.ac.id>
2026-04-29 11:09:11 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/regusync/regusync/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.regusync.regusync
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/onboarding/onboarding_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class OnboardingScreen extends StatefullWidget{
+    const OnboardingScreen({super.key});
+
+    @override
+    State<
+}
